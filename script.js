a18b8ca7b0d5fd4d5eaee878c2a0c6c4116f6f6e
r0hit-gupta <rohit1997gupta@gmail.com>
2016-12-31 14:52:47 +0530

    Changes

--- script.js
+++ script.js
@@ -1,55 +1,94 @@
 const gapikey = 'AIzaSyCKMpw2nmPnon_gkh4EIXnbiAmrZNw-v4M';
-
-function startListening(){
-
+var listening = false;
 // new instance of speech recognition
 var recognition = new webkitSpeechRecognition();
+
 // set params
 recognition.continuous = false;
 recognition.interimResults = true;
-recognition.start();
 
-recognition.onresult = function(event){
-  // delve into words detected results & get the latest
-  // total results detected
-  var resultsLength = event.results.length -1 ;
-  // get length of latest results
-  var ArrayLength = event.results[resultsLength].length -1;
-  // get last word detected
-  var saidWord = event.results[resultsLength][ArrayLength].transcript.substr(4);
-    console.log(saidWord);
+function startListening(){
+    recognition.start();
 
-  if(event.results[0].isFinal){
-  	playMusic(saidWord);
-    
-  console.log(saidWord);
-  var msg = new SpeechSynthesisUtterance("playing, " + saidWord);
-window.speechSynthesis.speak(msg);
+    recognition.onresult = function(event){
+        var saidWord = event.results[0][0].transcript.substr(4);
+        console.log(saidWord);
+        $('.text').text(saidWord);
+        if(event.results[0].isFinal){
+            $('.ring1, .ring2').removeClass('mic-active');
+             playMusic(saidWord); 
+            console.log(saidWord);
+            var msg = new SpeechSynthesisUtterance("playing, " + saidWord);
+            window.speechSynthesis.speak(msg);
+            noteFall();
+      }
+    }
+
+    // speech error handling
+    recognition.onerror = function(event){
+      console.log('Error occured');
+      console.log(event);
+    }
 }
-}
-
-// speech error handling
-recognition.onerror = function(event){
-  console.log('error?');
-  console.log(event);
-}
-
-var msg = new SpeechSynthesisUtterance('okay');
-window.speechSynthesis.speak(msg);
-
 
 function playMusic(song){
-	$.get(
-    	"https://www.googleapis.com/youtube/v3/search", {
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search", {
             part: 'snippet, id',
-            q: song + 'song',
+            q: song + ' song',
             type: 'video',
             key: gapikey
         }, function(data) {
-       var id = data.items[0].id.videoId;
-var url = 'https://www.youtube.com/embed/'+id+'?enablejsapi=1&autoplay=1';
+            var id = data.items[0].id.videoId;
+            var url = 'https://www.youtube.com/embed/'+id+'?enablejsapi=1&autoplay=1';
        
-     console.log(data.items[0].id.videoId);
-     $('iframe').attr('src', url);
+             console.log(data.items[0].id.videoId);
+             $('iframe').attr('src', url);
         }
 )}
+
+$('.mic').on('click', function(){
+    if(listening){
+        recognition.abort();
+          $('.ring1, .ring2').removeClass('mic-active');
+        listening = 0;
+    }
+    else {
+        listening = 1;
+          $('iframe').attr('src', '');
+  $('.ring1, .ring2').toggleClass('mic-active');
+    var msg = new SpeechSynthesisUtterance("ok");
+    window.speechSynthesis.speak(msg);
+    startListening();
+    }
+
+});
+
+//animated notes 
+function noteFall(){
+    var notes = document.querySelectorAll('.note');
+    for (var i = 0, len = notes.length; i < len; ++i) {
+        var note = notes[i];
+            var scale = Math.random() * .8 + .2;
+        var player = note.animate([
+          { transform: 'translate3d(' + (i/len*100) + 'vw,0,0) scale(' + scale + ')', opacity: scale*0.6 },
+          { transform: 'translate3d(' + (i/len*100 + 10) + 'vw,50vh,0) scale(' + scale + ')', opacity: 0 }
+        ], {
+          duration: Math.random() * 3000 + 2000,
+          iterations: Infinity,
+          delay: -(Math.random() * 5000)
+        });
+    }
+    $('.notefall').fadeIn(); 
+}
+
+$(document).on('change', 'iframe', function(){
+    if(this.attr('src')) noteFall();
+    else {
+        $('notefall').fadeOut();
+    }
+});
+
+
+
+
